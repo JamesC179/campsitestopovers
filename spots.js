8145a175d57db233b3831133b971d1b3395b7b7e
JamesC179 <jamescshearwater@gmail.com>
2026-07-14 09:30:22 +0100

    Update spots.js

--- spots.js
+++ spots.js
@@ -56,7 +56,7 @@ window.STOPOVERS = [
     price: "£15",
     rating: 4,
     facilities: ["Public Toilets"],
-    notes: "There are three car parks with Motorhome bays that you can book, we only stayed in the Beadnell, right by the beach and public toilets too, it said the toilets were locked overnight but they weren't. Toilets not great but better than nothing. Have to book your spot on hte council website",
+    notes: "There are three car parks with Motorhome bays that you can book, we only stayed in the Beadnell, right by the beach and public toilets too, it said the toilets were locked overnight but they weren't. Toilets not great but better than nothing. Have to book your spot on the council website",
     contact: { label: "https://www.northumberland.gov.uk/parking/council-car-parks/motorhome-parking", href: "https://www.northumberland.gov.uk/parking/council-car-parks/motorhome-parking" },
     addedBy: "James C",
     photos: ["https://jamesc179.github.io/campsitestopovers/images/beadnell_beach.jpeg"]
